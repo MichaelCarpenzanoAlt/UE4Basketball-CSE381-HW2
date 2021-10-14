020ca9c215d0c0319c18861db325c8a81e08db83
MichaelCarpenzano <mlcarpenzano@gmail.com>
2021-10-14 01:59:48 -0400

    added source and build files

---
+++ Source/Basketball/BasketballCharacter.cpp
@@ -0,0 +1,151 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BasketballCharacter.h"
+
+// Sets default values
+ABasketballCharacter::ABasketballCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// --- Configure First-Person Camera ---
+
+	// Create a first person camera component.
+	BasketballCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	check(BasketballCameraComponent != nullptr);
+
+	// Attach the camera component to out capule component.
+	BasketballCameraComponent->SetupAttachment(CastChecked<USceneComponent, UCapsuleComponent>(GetCapsuleComponent()));
+
+	// Position the camera slightly above the eyes.
+	BasketballCameraComponent->SetRelativeLocation(FVector(0.0f, 0.0f, 50.0f + BaseEyeHeight));
+
+	// Enable the pawn to control the camera rotation.
+	BasketballCameraComponent->bUsePawnControlRotation = true;
+
+	// --- Configure First-Person Mesh ---
+
+	// Create a first person mesh component for the owning player.
+	BasketballMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FirstPersonMesh"));
+	check(BasketballMesh != nullptr);
+
+	// Only the owning player sees this mesh.
+	BasketballMesh->SetOnlyOwnerSee(true);
+
+	// Attach the BasketballMesh (FPS mesh) to the BasketballCameraComponent (FPS camera).
+	BasketballMesh->SetupAttachment(BasketballCameraComponent);
+
+	// Disable some enviromental shadows to preserve the illusion of having a single mesh.
+	BasketballMesh->bCastDynamicShadow = false;
+	BasketballMesh->CastShadow = false;
+
+	// The owning player doesn't see the regular (third-person) body mesh.
+	GetMesh()->SetOwnerNoSee(true);
+}
+
+// Called when the game starts or when spawned
+void ABasketballCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	check(GEngine != nullptr);
+
+	// Display a debug message for five seconds.
+	// The -1 "Key" value argument prevents the message from being updated or refreshed.
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("We are using BasketballCharacter."));
+
+}
+
+// Called every frame
+void ABasketballCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABasketballCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// Set up "movement" bindings.
+	PlayerInputComponent->BindAxis("MoveForward", this, &ABasketballCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ABasketballCharacter::MoveRight);
+
+	// Set up "look" bindings.
+	PlayerInputComponent->BindAxis("Turn", this, &ABasketballCharacter::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &ABasketballCharacter::AddControllerPitchInput);
+
+	// Set up "action" bindings.
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ABasketballCharacter::StartJump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ABasketballCharacter::StopJump);
+
+	// Set up "fire" binding.
+	PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &ABasketballCharacter::Fire);
+}
+
+void ABasketballCharacter::MoveForward(float Value)
+{
+	// Find out which way is "forward" and record that the player wants to move that way.
+	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::X);
+	AddMovementInput(Direction, Value);
+
+}
+
+void ABasketballCharacter::MoveRight(float Value)
+{
+	// Find out which way is "right" and record that the player wants to move that way.
+	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::Y);
+	AddMovementInput(Direction, Value);
+
+}
+
+void ABasketballCharacter::StartJump()
+{
+    bPressedJump = true;
+}
+
+void ABasketballCharacter::StopJump()
+{
+    bPressedJump = false;
+}
+
+void ABasketballCharacter::Fire()
+{
+	// Attempt to fire a projectile.
+	if (ProjectileClass)
+	{
+		// Get the camera transform.
+		FVector CameraLocation;
+		FRotator CameraRotation;
+		GetActorEyesViewPoint(CameraLocation, CameraRotation);
+
+
+		// Set MuzzleOffset to spawn projectiles slightly in front of the camera.
+		MuzzleOffset.Set(100.0f, 0.0f, 0.0f);
+
+		// Transform MuzzleOffset from camera space to world space.
+		FVector MuzzleLocation = CameraLocation + FTransform(CameraRotation).TransformVector(MuzzleOffset);
+
+		// Skew the aim to be slightly upwards.
+		FRotator MuzzleRotation = CameraRotation;
+		MuzzleRotation.Pitch += 10.0f;
+
+		UWorld* World = GetWorld();
+		if (World)
+		{
+			FActorSpawnParameters SpawnParams;
+			SpawnParams.Owner = this;
+			SpawnParams.Instigator = GetInstigator();
+
+			// Spawn the projectile at the muzzle.
+			AFPSProjectile* Projectile = World->SpawnActor<AFPSProjectile>(ProjectileClass, MuzzleLocation, MuzzleRotation, SpawnParams);
+			if (Projectile)
+			{
+				// Set the projectile's initial trajectory.
+				FVector LaunchDirection = MuzzleRotation.Vector();
+				Projectile->FireInDirection(LaunchDirection);
+			}
+		}
+	}
+}
---
+++ Source/Basketball/BasketballCharacter.h
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "FPSProjectile.h"
+#include "BasketballCharacter.generated.h"
+
+UCLASS()
+class BASKETBALL_API ABasketballCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ABasketballCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Projectile class to spawn.
+	UPROPERTY(EditDefaultsOnly, Category = Projectile)
+		TSubclassOf<class AFPSProjectile> ProjectileClass;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// Handles input for moving forward and backward.
+	UFUNCTION()
+		void MoveForward(float Value);
+
+	// Handles input for moving right and left.
+	UFUNCTION()
+		void MoveRight(float Value);
+
+	// Sets jump flag when key is pressed.
+	UFUNCTION()
+		void StartJump();
+
+	// Clears jump flag when key is released.
+	UFUNCTION()
+		void StopJump();
+
+	// Function that handles firing projectiles.
+	UFUNCTION()
+		void Fire();
+
+	// Basketball (FPS) Camera
+	UPROPERTY(VisibleAnywhere)
+		UCameraComponent* BasketballCameraComponent;
+
+	// First-person mesh (arms), visible only to the owning player
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		USkeletalMeshComponent* BasketballMesh;
+
+	// Gun muzzle offset from the camera location.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+		FVector MuzzleOffset;
+};
